b472c22c951521d138e6da97a316a8a114bdb281
Your Name <you@example.com>
2018-05-09 16:59:12 +0200

    file lobby_notifications.js added

---
+++ gui/common/lobby_notifications.js
@@ -0,0 +1,48 @@
+/**
+ * Indicates if lobby dialog is opened.
+ */
+var g_LobbyDialogOpened = false;
+
+/**
+ * Function name for call when notification happens in lobby.
+ */
+var g_UpdateLobbyNotification = notify => {};
+
+/**
+ * Notification sign used in menus.
+ */
+var g_NofiticationSign = "*";
+
+/**
+ * Lobby net messages, that triggers a notification.
+ */
+var g_LobbyNetMessageTypeNotification = {
+	"system": {
+		"disconnected": msg => true
+	},
+	"chat": {
+        "subject": msg => true,
+        "role": msg => true,
+        "room-message": msg => msg.text.toLowerCase().indexOf(Engine.LobbyGetNick().toLowerCase()) != -1,
+        "kicked": msg => msg.nick == Engine.LobbyGetNick(),
+        "banned": msg => msg.nick == Engine.LobbyGetNick(),
+        "private-message": msg => true
+	}
+};
+
+function handleNetLobbyMessagesInBackground()
+{
+    if (!g_LobbyDialogOpened)
+    {
+        let msg = Engine.LobbyGuiPollNewMessage(); // Only poll new messages no historic.
+        if (msg && g_LobbyNetMessageTypeNotification[msg.type] &&
+            g_LobbyNetMessageTypeNotification[msg.type][msg.level] &&
+            g_LobbyNetMessageTypeNotification[msg.type][msg.level](msg))
+            g_UpdateLobbyNotification(true);
+    }
+}
+
+function setLobbyDialogClosed()
+{
+	g_LobbyDialogOpened = false;
+}
