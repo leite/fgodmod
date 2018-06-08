13d64c385fd76f1912bd7813326373e41a218dd7
ff <ff@ff.de>
2018-06-08 19:01:14 +0200

    files added

---
+++ gui/manual/manual.js
@@ -0,0 +1,45 @@
+var hasCallback = false;
+var g_Page = "";
+var g_Title = translate("Information");
+var g_OpenPage = "manual"
+
+function init(data)
+{
+	g_Page = data.page;
+	if (data.callback)
+		hasCallback = true;
+	if (data.title)
+		g_Title = data.title
+	if (data.url)
+	{
+		var urlButton = Engine.GetGUIObjectByName("url");
+		var callback = function(url)
+		{
+			return function()
+			openURL(url);
+		}(data.url);
+		urlButton.onPress = callback;
+		urlButton.hidden = false;
+	}
+	if (data.openPage)
+		g_OpenPage = data.openPage;
+	showPage(g_OpenPage);
+}
+
+function showPage(page)
+{
+	g_OpenPage = page;
+	Engine.GetGUIObjectByName("mainText").caption = page == "fgod" ? Engine.TranslateLines(Engine.ReadFile("gui/manual/README.md")) :
+		Engine.TranslateLines(Engine.ReadFile("gui/" + g_Page + ".txt"));
+	Engine.GetGUIObjectByName("title").caption = page == "fgod" ? "Fgod Readme" : g_Title;
+	Engine.GetGUIObjectByName("pageSwitchButton").caption = page == "fgod" ? g_Title : "Fgod Readme";
+	Engine.GetGUIObjectByName("pageSwitchButton").onPress = () => showPage(page == "fgod" ? "manual" : "fgod");
+}
+
+function closeManual()
+{
+	if (hasCallback)
+		Engine.PopGuiPageCB(g_OpenPage);
+	else
+		Engine.PopGuiPage();
+}
